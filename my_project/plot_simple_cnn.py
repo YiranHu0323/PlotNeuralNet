0b57802688d59bffc477bf7dd585006e983c24a4
YiranHu0323 <yh3639@columbia.edu>
2024-08-27 13:36:09 -0400

    change model structure

--- my_project/plot_simple_cnn.py
+++ my_project/plot_simple_cnn.py
@@ -10,7 +10,7 @@ def plot_simple_cnn():
         to_begin(),
 
         # Input
-        to_input('input.jpg', width=64, height=64, caption='224x224x3'),
+        to_input('input.jpg', width=64, height=64),
         
         # Encoder
         to_Conv("conv1", 224, 64, offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=2, caption="Conv1 + BN"),
